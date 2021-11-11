b5074257f312b74f25e5f7165ce7864edc2f5274
Christopher Briceño <82926376+ChristoperBA@users.noreply.github.com>
2021-11-11 13:43:29 -0600

    Clases Vuelos y Asientos
    Valeria Chacon

---
+++ Proyecto-CheckIn-CheckOut/src/proyecto/checkin/checkout/Asientos.java
@@ -0,0 +1,25 @@
+/*
+
+ */
+package proyecto.checkin.checkout;
+
+import javax.swing.JOptionPane;
+
+
+public class Asientos 
+{
+    //Atributos
+            private String Pasajeros;
+            private String ID;
+            private String Ocupados;
+            private String Libres;
+            
+            //metodo Mensajes 
+            public void MensajeC()
+              {
+               JOptionPane.showMessageDialog(null, "Clase Asientos");  
+              }
+    
+    
+   
+}
---
+++ Proyecto-CheckIn-CheckOut/src/proyecto/checkin/checkout/Vuelos.java
@@ -0,0 +1,34 @@
+/*
+
+ */
+package proyecto.checkin.checkout;
+
+import javax.swing.JOptionPane;
+
+
+public class Vuelos 
+{
+     //Atributos
+     private String Nombre;
+     private String ID;
+     private String Salida;
+     private String Llegada;
+            
+    //metodo Mensajes 
+    public void MensajeB()
+    {
+        JOptionPane.showMessageDialog(null, "Clase Vuelos");  
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
