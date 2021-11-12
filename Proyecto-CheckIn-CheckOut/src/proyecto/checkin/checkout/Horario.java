5fd6b0f2ff2b9bdfebd055f073a5c5e6797069b6
jdpala <94072797+jdpala@users.noreply.github.com>
2021-11-11 21:05:35 -0600

    Add files via upload

---
+++ Proyecto-CheckIn-CheckOut/src/proyecto/checkin/checkout/Horario.java
@@ -0,0 +1,19 @@
+
+package proyecto.checkin.checkout;
+
+import javax.swing.JOptionPane;
+public class Horario 
+{
+   //Atributos
+    private int hora;
+    private String ID;
+    private String NVuelo;
+
+    
+    //Metodo Mensajes
+    public void MensajeA()
+    {
+        JOptionPane.showMessageDialog(null,"Clase Horarios");
+    } 
+    
+}
