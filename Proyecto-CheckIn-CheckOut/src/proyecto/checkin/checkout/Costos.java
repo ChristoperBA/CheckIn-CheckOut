230fb79df26673e1f2dc854dfb4b993300717d45
valechq19 <94030653+valechq19@users.noreply.github.com>
2021-12-09 01:06:59 -0600

    Update Costos.java

--- Proyecto-CheckIn-CheckOut/src/proyecto/checkin/checkout/Costos.java
+++ Proyecto-CheckIn-CheckOut/src/proyecto/checkin/checkout/Costos.java
@@ -17,4 +17,32 @@ public class Costos
         JOptionPane.showMessageDialog(null,"Clase Costos");
     }
 }*/
+    public class Costos 
+{
+    
+    //Atributos
+    private int valor = 250;
+    private String ID;
+    private String NAsientos;
+    private int Total; 
+    private String Nombre;
+    
+    //Metodo Mensajes
+    public void MensajeA()
+    {
+        javax.swing.JOptionPane.showMessageDialog(null,"Clase Costos");
+    }
+    
+    public void Factura()
+    {
+       NAsientos = Integer.parseInt(javax.swing.JOptionPane.showInputDialog("Digite el número de asientos que agendó:"));
+       Nombre = javax.swing.JOptionPane.showInputDialog("Digite su nombre: ");
+       ID = Integer.parseInt(javax.swing.JOptionPane.showInputDialog("Digite su ID: "));
+       Total = valor * NAsientos
+     }
+        public void Mensaje1()
+    {
+        javax.swing.JOptionPane.showMessageDialog(null,"Nombre: " + Nombre +"ID: " + ID + "Su número de asientos reservados: " + NAsientos +"El total de su compra es de:$ " + Total);
+    }
+}
 }
